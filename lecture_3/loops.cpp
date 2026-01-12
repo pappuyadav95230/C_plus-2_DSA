20aab739427dfc2504d06583b658726eb76d9839
pappu@yadav <610490papu@gmail.com>
2026-01-12 22:53:46 +0530

    Add implementation of do-while loop example in loops.cpp

---
+++ lecture_3/loops.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// for loops
+// int main()
+// {
+//     int i = 1;
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         cout << i <<" ";
+//     }
+// }
+
+// while loops
+// int main()
+// {
+//     int i = 1;
+//     while (i <= 5)
+//     {
+//         cout << i << " ";
+//         i++;
+//     }
+// }
+
+// do while loops
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << i << " ";
+        i++;
+    } while (i <= 5);
+}

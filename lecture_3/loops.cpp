5ce78a38b38ece95ee9e651a2c03635b82191017
pappu@yadav <610490papu@gmail.com>
2026-01-12 23:24:32 +0530

    Refactor loops.cpp to comment out unused loop examples and improve code organization

--- lecture_3/loops.cpp
+++ lecture_3/loops.cpp
@@ -11,6 +11,48 @@ using namespace std;
 //     }
 // }
 
+//  for loop
+// int main()
+// {
+//     for (int i = 5; i > 0; i = i - 1)
+//     {
+//         cout << i << " ";
+//     }
+// }
+
+// print table
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         cout << "19 * " << i << " = " << i * 19 << " " << endl;
+//     }
+// }
+
+// what is neccessery to write in the loop
+// int main()
+// {
+//     int i = 0;
+//     for (; ; )
+//     {
+//         if (i < 5)
+//         {
+//             cout << i <<endl;
+//             i++;
+//         }
+//     }
+// }
+
+//
+int main()
+{
+    int n;
+    if (cin >> n)
+    {
+        cout << "Pappu";
+    }
+}
+
 // while loops
 // int main()
 // {
@@ -23,12 +65,12 @@ using namespace std;
 // }
 
 // do while loops
-int main()
-{
-    int i = 1;
-    do
-    {
-        cout << i << " ";
-        i++;
-    } while (i <= 5);
-}
+// int main()
+// {
+//     int i = 1;
+//     do
+//     {
+//         cout << i << " ";
+//         i++;
+//     } while (i <= 5);
+// }

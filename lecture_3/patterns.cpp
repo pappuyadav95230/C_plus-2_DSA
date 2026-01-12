5ad737f692776db8bb654641be4f2f69d0121869
pappu@yadav <610490papu@gmail.com>
2026-01-12 23:51:59 +0530

    Add square pattern implementation in patterns.cpp

---
+++ lecture_3/patterns.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// sold rectangle
+// int main()
+// {
+//     // outer loop - row obeserve
+//     for (int row = 0; row < 3; row++)
+//     {
+//         // inner loop - column obeserve
+//         for (int col = 0; col < 5; col++)
+//         {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+
+// }
+
+// squre pattern
+int main()
+{
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout << " * "; 
+        }
+        cout << endl;
+    }
+}

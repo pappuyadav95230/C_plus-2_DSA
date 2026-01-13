31c014e1148dc74acb5c40ed58a28aabc787168d
pappu@yadav <610490papu@gmail.com>
2026-01-13 22:21:02 +0530

    Refactor patterns.cpp to comment out unused square pattern implementation and enhance hollow rectangle pattern

--- lecture_3/patterns.cpp
+++ lecture_3/patterns.cpp
@@ -33,19 +33,35 @@ using namespace std;
 //     }
 // }
 
-// hollow squre pattern
-int main()
-{
-    int n;
-    cout << "Enter the number : ";
-    cin >> n;
+// squre pattern with input
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
 
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < n; col++)
-        {
-            cout << " * ";
+//     for (int row = 0; row < n; row++)
+//     {
+//         for (int col = 0; col < n; col++)
+//         {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// Hollow Rectangle
+int main(){
+    for(int row=0; row<3; row++){
+        for(int col=0; col<5; col++){
+            if(row==0 || row==2 || col==0 || col==4){
+                cout<<" * ";
+            }
+            else{
+                cout<<"   ";
+            }
         }
-        cout << endl;
+        cout<<endl;
     }
 }

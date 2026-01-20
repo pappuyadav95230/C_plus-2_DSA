b8aab5a65fecdf88a73043ca8ebf0c194b7d6362
pappu@yadav <610490papu@gmail.com>
2026-01-20 21:40:44 +0530

    Add implementation for pre and post increment/decrement operations in pre-post.cpp

---
+++ lecture_4/pre-post.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    // post increment
+    cout << a++ << endl;   // a=5 then increment ho kar 6 ho gya 
+    // pre increment
+    cout << ++a << endl;  // pre incremnt ho kar 7 then print kiy 7
+    // post decrement
+    cout << a-- << endl;  // pahle print kiya 7 then - decrement huaa 6 
+    // pre decrement
+    cout << --a << endl;  // pahle decremnt huaa then 5 ho ya or print ho gya 5 
+}

3c0bee0dc4a494aa89c5e7bd11d69a22f49c8120
pappu@yadav <610490papu@gmail.com>
2026-01-21 22:15:19 +0530

    Add PrintName function to display user-defined number of names

---
+++ lecture_5/funtion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void PrintName()
+{
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Pappu" << endl;
+    }
+}
+
+int main()
+{
+    PrintName();
+}

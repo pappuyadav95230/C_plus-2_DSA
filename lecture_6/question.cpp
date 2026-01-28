811b0425f2eaa8bb0a9c32654bd8180423df6100
pappu@yadav <610490papu@gmail.com>
2026-01-28 22:24:08 +0530

    Refactor question.cpp to print an array of digits as a single number

--- lecture_6/question.cpp
+++ lecture_6/question.cpp
@@ -45,24 +45,41 @@
 //     return 0;
 // }
 
-// print 567 -> 765 try 
+// print 567 -> 765 try
+// #include <iostream>
+// using namespace std;
+
+// int printNumber(int n)
+// {
+//     int ans = 0;
+//     int digit = ans * 10 + n;
+//     cout << digit;
+//     return digit;
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
+//     printNumber(n);
+
+//     return 0;
+// }
+
+// final we need to pritn {8,2,3,7} -> 8237
 #include <iostream>
 using namespace std;
 
-int printNumber(int n)
-{
-    int ans = 0;
-    int digit = ans * 10 + n;
-    cout << digit;
-    return digit;
-}
-
 int main()
 {
-    int n;
-    cout << "Enter the number : ";
-    cin >> n;
-    printNumber(n);
+    int digit[] = {8, 2, 3, 7};
 
-    return 0;
+    int ans = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        ans = ans * 10 + digit[i];
+    }
+    cout << ans;
 }

212fff3c8d40515b24888d12af4852dcfb91d3d7
pappu@yadav <610490papu@gmail.com>
2026-01-28 21:59:54 +0530

    Refactor print function to display a number and update main function accordingly

--- lecture_6/question.cpp
+++ lecture_6/question.cpp
@@ -1,14 +1,39 @@
 // Print digest of a number
+// #include <iostream>
+// using namespace std;
+
+// void printDigest(int n)
+// {
+//     while (n != 0)
+//     {
+//         int digit = n % 10;
+//         cout << digit;
+//         n = n / 10;
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
+//     printDigest(n);
+//     return 0;
+// }
+
+
+// print 567 -> 567 i just try not sure 
 #include <iostream>
 using namespace std;
 
-void printDigest(int n)
+void printNumber(int n)
 {
     while (n != 0)
     {
-        int digit = n % 10;
+        int ans = 0;
+        int digit = ans * 10 + n;
         cout << digit;
-        n = n / 10;
+        break;
     }
 }
 
@@ -17,6 +42,6 @@ int main()
     int n;
     cout << "Enter the number : ";
     cin >> n;
-    printDigest(n);
+    printNumber(n);
     return 0;
 }

0fce2c087533620cc19603ec32cb25ee8cf7e88a
pappu@yadav <610490papu@gmail.com>
2026-02-26 22:44:33 +0530

    Refactor array input handling and output to display double of input numbers

--- lecture_8_Array/array.cpp
+++ lecture_8_Array/array.cpp
@@ -11,20 +11,98 @@
 //     return 0;
 // }
 
-// sum of elements
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int arr[] = {1, 2, 3, 4, 5, 8, 9};
+
+//     // printing all value
+//     for (int i = 0; i < 6; i++)
+//     {
+//         cout << arr[i] << endl;
+//     }
+// }
+
+// taking input of the array
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[10];
+//     cout << "Enter the number of the array ";
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         // int n;
+//         // cin >> n;
+//         // arr[i] = n;
+//         cin >> arr[i];
+//     }
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// take input and print double of the number of the array
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[10];
+//     cout << "Enter the number of the array ";
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cout << arr[i] * 2 << " " << endl;
+//     }
+// }
+
+// taking input from the user
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int ans = 0;
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[50];
+    int n;
+    cout << "How many numbers you want to add in array" << endl;
+    cin >> n;
 
-    for (int i = 0; i < 5; i++)
+    cout << "Enter the number " << endl;
+    for (int i = 0; i < n; i++)
     {
-        ans = ans + arr[i];
+        cin >> arr[i];
     }
-    cout << ans;
+    cout << "double of the numbers are:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] * 2 << endl;
+    }
     return 0;
 }
+// sum of elements
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int ans = 0;
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         ans = ans + arr[i];
+//     }
+//     cout << ans;
+//     return 0;
+// }

05596f3f642bfd8db188c296053cedbe165f7807
pappu@yadav <610490papu@gmail.com>
2026-02-27 07:45:12 +0530

    Refactor array input handling and add functions for array manipulation and printing

--- lecture_8_Array/array.cpp
+++ lecture_8_Array/array.cpp
@@ -68,27 +68,53 @@
 // }
 
 // taking input from the user
-#include <iostream>
-using namespace std;
-int main()
-{
-    int arr[50];
-    int n;
-    cout << "How many numbers you want to add in array" << endl;
-    cin >> n;
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[50];
+//     int n;
+//     cout << "How many numbers you want to add in array" << endl;
+//     cin >> n;
 
-    cout << "Enter the number " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    cout << "double of the numbers are:"<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] * 2 << endl;
-    }
-    return 0;
-}
+//     cout << "Enter the number " << endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     cout << "double of the numbers are:"<<endl;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] * 2 << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     for (int i = 0; i < 5; i++)
+
+//     {
+//         arr[i] = 1;
+//         cout << arr[i]<<" " ;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[10];
+//     memset(arr, 2, sizeof(arr));
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
 // sum of elements
 // #include <iostream>
 // using namespace std;
@@ -106,3 +132,35 @@ int main()
 //     cout << ans;
 //     return 0;
 // }
+
+// Array and function
+#include <iostream>
+using namespace std;
+
+int printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+void inc(int arr[], int size)
+{
+    arr[0] = arr[0] + 10;
+
+    printArray(arr, size);
+}
+
+int main()
+{
+    int arr[] = {5,6};
+    int size = 2;
+
+    inc(arr, size);
+
+    printArray(arr, size);
+    return 0;
+}

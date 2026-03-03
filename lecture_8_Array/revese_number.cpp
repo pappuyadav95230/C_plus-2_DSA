61f5a0742974b28edea781cd65437d7737fe8b1b
pappu@yadav <610490papu@gmail.com>
2026-03-03 22:28:03 +0530

    Add revese_number.cpp to reverse elements in an array

---
+++ lecture_8_Array/revese_number.cpp
@@ -0,0 +1,23 @@
+// Reverse number in array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = 6;
+
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++; 
+        end--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}

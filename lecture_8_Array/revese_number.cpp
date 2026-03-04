ec59665fc6737e7e56d8aea3ef54baceadcbe927
pappu@yadav <610490papu@gmail.com>
2026-03-04 12:17:18 +0530

    Refactor revese_number.cpp to remove unused code and adjust array size for even elements

--- lecture_8_Array/revese_number.cpp
+++ lecture_8_Array/revese_number.cpp
@@ -1,11 +1,38 @@
 // Reverse number in array
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5, 6, 7};
+//     int size = 7;
+
+//     int start = 0;
+//     int end = size - 1;
+
+//     while (start < end)
+//     {
+//         swap(arr[start], arr[end]);
+//         start++; 
+//         end--;
+//     }
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
+
+
+
+
+// for even number
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    int size = 7;
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = 6;
 
     int start = 0;
     int end = size - 1;

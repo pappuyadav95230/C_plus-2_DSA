28921e1d12b39ecf1e30c0db18440da6104a6edf
pappu@yadav <610490papu@gmail.com>
2026-03-05 07:47:47 +0530

    Fix intersection logic by adding duplicate element handling in intersection_of_two_array.cpp

--- lecture_9_vector/intersection_of_two_array.cpp
+++ lecture_9_vector/intersection_of_two_array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr{1, 2, 3, 4, 6, 8};
+    vector<int> arr{1, 2, 3,3, 4, 6, 8};
     vector<int> brr{3, 4, 9, 10};
 
     vector<int> ans;
@@ -17,7 +17,8 @@ int main()
         for (int j = 0; j < brr.size(); j++)
         {
             if (element == brr[j])
-            {
+            {   
+                brr[j] = -1;
                 ans.push_back(element);
             }
         }

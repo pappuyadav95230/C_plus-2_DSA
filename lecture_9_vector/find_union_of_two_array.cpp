1e73ab0a9334eb74b7e41c93f13873520d984159
pappu@yadav <610490papu@gmail.com>
2026-03-05 07:47:27 +0530

    Add find_union_of_two_array.cpp to implement union of two arrays using vectors

---
+++ lecture_9_vector/find_union_of_two_array.cpp
@@ -0,0 +1,30 @@
+// Assume duplicate element is not there in array
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 3, 5, 7, 9};
+    int sizea = 5;
+    int brr[] = {2, 4, 6, 8, 10};
+    int sizeb = 5;
+
+    vector<int> ans;
+
+    for (int i = 0; i < sizea; i++)
+    {
+        ans.push_back(arr[i]);
+    }
+
+    for (int i = 0; i < sizeb; i++)
+    {
+        ans.push_back(brr[i]);
+    }
+
+    // printing array
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+}
---
+++ lecture_9_vector/intersection_of_two_array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr{1, 2, 3, 4, 6, 8};
+    vector<int> brr{3, 4, 9, 10};
+
+    vector<int> ans;
+
+    // outer loop for arr
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int element = arr[i];
+        // inner loop for the brr
+        for (int j = 0; j < brr.size(); j++)
+        {
+            if (element == brr[j])
+            {
+                ans.push_back(element);
+            }
+        }
+    }
+    // printing intersection
+    // for (int i = 0; i < ans.size(); i++)
+    // {
+    //     cout << ans[i] << " ";
+    // }
+
+    // second method to print
+    for (auto value : ans)
+    {
+        cout << value << " ";
+    }
+    return 0;
+}

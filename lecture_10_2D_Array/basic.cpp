f56f93c7605de2ef4b1b97b1ac7d27c3fc0a01be
pappu@yadav <610490papu@gmail.com>
2026-03-09 08:03:04 +0530

    Fix variable name and enhance output formatting in basic.cpp for 2D array demonstration

--- lecture_10_2D_Array/basic.cpp
+++ lecture_10_2D_Array/basic.cpp
@@ -4,13 +4,32 @@ using namespace std;
 int main()
 {
     // int arr[2][3];
-
-    int brr[3][3] =
+    int arr[3][3] =
         {
             {1, 2, 3},
             {4, 5, 6},
             {5, 6, 9}};
-
-    cout << brr[2][2];
+    // outer loop for row
+    cout << "Printing Row wise" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        // inner loop for column
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " "; 
+        }
+        cout << endl;
+    }
+    cout << "Printing column wise" << endl;
+    // coloumn wise
+    for (int i = 0; i < 3; i++)
+    {
+        // inner loop for column
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }

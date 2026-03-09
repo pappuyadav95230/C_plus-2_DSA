1701910ac438f7c40e95f87260355c43096cdd00
pappu@yadav <610490papu@gmail.com>
2026-03-09 07:45:25 +0530

    Add basic.cpp to demonstrate 2D array initialization and element access

---
+++ lecture_10_2D_Array/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int arr[2][3];
+
+    int brr[3][3] =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {5, 6, 9}};
+
+    cout << brr[2][2];
+    return 0;
+}

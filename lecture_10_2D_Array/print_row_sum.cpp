b2b56cb524f58f0a371413194e04e5f73bb899f8
pappu@yadav <610490papu@gmail.com>
2026-03-10 22:01:46 +0530

    Add print_row_sum.cpp to implement a program that calculates and prints the sum of each row in a 2D array

---
+++ lecture_10_2D_Array/print_row_sum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Function to print the sum of the row
+void printRowWiseSum(int arr[][3], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < cols; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+        cout << sum << endl;
+    }
+}
+
+int main()
+{
+    int arr[3][3];
+    int rows = 3;
+    int cols = 3;
+
+    // input
+    cout << "Enter the array elements" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    // printing
+    cout << "Printing row wise" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "printing sum row wise" << endl;
+    printRowWiseSum(arr, rows, cols);
+    return 0;
+}

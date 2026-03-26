dadf296fc4a2814a5ab9a5e9fa4f6edd413445fb
pappu@yadav <610490papu@gmail.com>
2026-03-26 08:03:33 +0530

    Refactor first occurrence search to use lower_bound for improved efficiency

--- lecture_11_serching_sorting.cpp/find_first_occrence_with_vector.cpp
+++ lecture_11_serching_sorting.cpp/find_first_occrence_with_vector.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int firstOccurence(vector<int> v, int target)
-{
-    int start = 0;
-    int end = v.size() - 1;
-    int ans = -1;
+// int firstOccurence(vector<int> v, int target)
+// {
+//     int start = 0;
+//     int end = v.size() - 1;
+//     int ans = -1;
 
-    while (start <= end)
-    {
-        int mid = start + (end - start) / 2;
-        int element = v[mid];
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+//         int element = v[mid];
 
-        if (element == target)
-        {
-            ans = mid;
-            end = mid - 1; // move left
-        }
-        else if (target < element)
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-    }
-    return ans;
-}
+//         if (element == target)
+//         {
+//             ans = mid;
+//             end = mid - 1; // move left
+//         }
+//         else if (target < element)
+//         {
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             start = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> v{1, 2, 3, 4, 4, 4, 4, 6, 7, 9};
+//     int target = 4;
+
+//     int indexOftarget = firstOccurence(v, target);
+//     cout << "target found at index: " << indexOftarget << endl;
+//     return 0;
+// }
 
 int main()
 {
     vector<int> v{1, 2, 3, 4, 4, 4, 4, 6, 7, 9};
     int target = 4;
 
-    int indexOftarget = firstOccurence(v, target);
-    cout << "target found at index: " << indexOftarget << endl;
+    auto ans = lower_bound(v.begin(), v.end(), target);
+
+    cout << "target found at index: " << ans - v.begin() << endl;
     return 0;
 }

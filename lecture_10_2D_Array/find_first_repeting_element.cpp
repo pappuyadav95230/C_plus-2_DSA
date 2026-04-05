3e4a1eca656f8869d0196a972e0f1f9a76d8c4d3
pappu@yadav <610490papu@gmail.com>
2026-04-05 18:30:38 +0530

    Add commented code examples for first repeating element solution using vector and hashmap

--- lecture_10_2D_Array/find_first_repeting_element.cpp
+++ lecture_10_2D_Array/find_first_repeting_element.cpp
@@ -27,3 +27,41 @@ int main()
     int result = firstRepeating(arr, n);
     cout << result << endl;
 }
+
+
+// on GFG 
+// class Solution {
+//   public:
+//     int firstRepeated(vector<int> &arr) {
+//         // code here
+//         int n = arr.size();
+//         for(int i =0; i<n; i++){
+//             bool isRepeted = false;
+//             for(int j = i+1; j<n; j++){
+//                 if(arr[i] == arr[j]){
+//                     isRepeted = true;
+//                     return i+1;
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+// using hashmap
+//  unordered_map<int, int> hash;
+
+//         // Count frequency
+//         for(int i = 0; i < arr.size(); i++){
+//             hash[arr[i]]++;
+//         }
+
+//         // Find first repeating index
+//         for(int i = 0; i < arr.size(); i++){
+//             if(hash[arr[i]] > 1){
+//                 return i + 1; // 1-based index
+//             }
+//         }
+
+//         return -1;

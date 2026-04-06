ad70e42b1c42bcd37baca53198c91a2f6076e306
pappu@yadav <610490papu@gmail.com>
2026-04-06 10:30:53 +0530

    Add implementation for finding common elements in three arrays without extra data structures

--- lecture_10_2D_Array/common_element_in_3_array.cpp
+++ lecture_10_2D_Array/common_element_in_3_array.cpp
@@ -21,3 +21,38 @@
 //             ans.push_back(i);
 //         }
 //         return ans;
+
+
+
+
+// whoout using extra data structure remove duplicate
+//  int i = 0, j = 0, k = 0;
+//         vector<int> ans;
+
+//         while (i < arr1.size() && j < arr2.size() && k < arr3.size()) {
+
+//             // If all equal → common element
+//             if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+//                 ans.push_back(arr1[i]);
+
+//                 int val = arr1[i];
+
+//                 // skip duplicates in all arrays
+//                 while (i < arr1.size() && arr1[i] == val) i++;
+//                 while (j < arr2.size() && arr2[j] == val) j++;
+//                 while (k < arr3.size() && arr3[k] == val) k++;
+//             }
+
+//             // Move the smallest pointer
+//             else if (arr1[i] < arr2[j]) {
+//                 i++;
+//             }
+//             else if (arr2[j] < arr3[k]) {
+//                 j++;
+//             }
+//             else {
+//                 k++;
+//             }
+//         }
+
+//         return ans;

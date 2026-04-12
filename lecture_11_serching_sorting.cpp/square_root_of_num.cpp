c82ebfa3fc2dfd5475f199c53a98c0035f056a76
pappu@yadav <610490papu@gmail.com>
2026-04-12 09:47:08 +0530

    Add floating point precision calculation to square root implementation

--- lecture_11_serching_sorting.cpp/square_root_of_num.cpp
+++ lecture_11_serching_sorting.cpp/square_root_of_num.cpp
@@ -1,3 +1,44 @@
+// #include <iostream>
+// using namespace std;
+
+// int sqrt(int n)
+// {
+//     int target = n;
+//     int start = 0;
+//     int end = n;
+//     int ans = -1;
+//     while (start <= end)
+//     {
+//         int mid = start + (end - start) / 2;
+//         if (mid * mid == target)
+//         {
+//             return mid;
+//         }
+//         else if (mid * mid < target)
+//         {
+//             ans = mid; //store ans
+//             start = mid + 1;
+//         }
+//         else
+//         {
+//             end = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     int n;
+//     cout << "Enter number ";
+//     cin >> n;
+
+//     int result = sqrt(n);
+//     cout << result;
+//     return 0;
+// }
+
+// Also find the floting point
 #include <iostream>
 using namespace std;
 
@@ -16,7 +57,7 @@ int sqrt(int n)
         }
         else if (mid * mid < target)
         {
-            ans = mid;
+            ans = mid; // store ans
             start = mid + 1;
         }
         else
@@ -33,7 +74,25 @@ int main()
     cout << "Enter number ";
     cin >> n;
 
-    int result = sqrt(n);
-    cout << result;
+    int ans = sqrt(n);
+    cout << "Ans is =" << ans << endl;
+
+    int precision;
+    cout << "Enter the number floting digits in precision" << endl;
+    cin >> precision;
+
+    double step = 0.1;
+    double finalAns = ans;
+
+    for (int i = 0; i < precision; i++)
+    {
+        for (double j = finalAns; j * j <= n; j = j + step)
+        {
+            finalAns = j;
+        }
+        step = step / 10;
+    }
+    cout << "Final ans is =" << finalAns << endl;
+
     return 0;
 }
